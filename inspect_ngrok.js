dcc9a3be5df7f5d3ec393173861635b5c710456b
yungb <yungb@users.noreply.github.com>
2026-05-09 16:00:35 -0400

    Remove inspect_ngrok.js

--- inspect_ngrok.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const dir = path.join(__dirname, 'node_modules', 'ngrok');
-try {
-  const files = fs.readdirSync(dir);
-  console.log(files.join('\n'));
-} catch (err) {
-  console.error('ERROR', err.message);
-}
